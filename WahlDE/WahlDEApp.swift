48421cd766a7e491459f4d0a2b66692d099d3dd3
Clemens05 <clemensrustemeier@gmail.com>
2023-05-30 14:41:26 +0200

    Initial Commit

---
+++ WahlDE/WahlDEApp.swift
@@ -0,0 +1,17 @@
+//
+//  WahlDEApp.swift
+//  WahlDE
+//
+//  Created by Clemens Rustemeier on 30.05.23.
+//
+
+import SwiftUI
+
+@main
+struct WahlDEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
